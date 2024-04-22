e44aee0788d2a64d31f5b364c57c9e62eeb92d41
prism4291 <densikousaku7@gmail.com>
2024-04-23 00:50:18 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import discord
 import dotenv
 import os
+import datetime
+import pytz
 
 from server import server_thread
 
@@ -11,6 +13,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 client = discord.Client(intents=intents)
+
+xxx=0
+
+@tasks.loop(seconds=50)
+async def loop():
+    global xxx
+    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+    if now.hour == 1 and now.minute == 1:
+        if xxx==0:
+            ch=await client.fetch_channel(927206819116490793)
+            await ch.send('<@&928999554290970634>')
+        xxx=1
+    else:
+        xxx=0
 
 @client.event
 async def on_message(message):
