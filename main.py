6534fb58b2cc40155142994531f00baa0a908497
prism4291 <densikousaku7@gmail.com>
2024-04-23 00:18:08 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import dotenv
+
+from server import server_thread
+
+dotenv.load_dotenv()
+
+TOKEN = os.environ.get("TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# Koyeb用 サーバー立ち上げ
+server_thread()
+client.run(TOKEN)
